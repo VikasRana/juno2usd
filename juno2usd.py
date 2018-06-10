e4642064f310b7a8215f76204c3e672916a609ba
VikasRana <VikasRana@users.noreply.github.com>
2018-06-10 12:18:31 +0530

    Update juno2usd.py
    Updated: Now shows the release price in GBP before converting to USD.

--- juno2usd.py
+++ juno2usd.py
@@ -2,26 +2,29 @@
 
 import requests
 from bs4 import BeautifulSoup
-from decimal import Decimal
 
-request = requests.get('https://www.xe.com/currencyconverter/convert/?Amount=1&From=GBP&To=USD')
+request = requests.get('https://www.xe.com/currencyconverter/convert/?Amount=1&From=GBP&To=USD') #exchange rate
 content = request.content
 soup = BeautifulSoup(content, "html.parser")
 element = soup.find("span", {"class": "uccResultAmount"})
 exrate = element.text
 print("Current exchange rate from GBP to USD is {}. (Reference:: www.xe.com)".format(exrate))
 print('\n')
+
+#Juno part from lines below
 print("Please paste below the www.JunoDownload.com URL and press ENTER.")
 user_url = input() #JunoDownload URL
 request = requests.get(user_url)
 content = request.content
 soup = BeautifulSoup(content, "html.parser")
+
 element = soup.find("span", {"class": "price"})
 str_price = element.text
-print('Found release price -> {}.'.format(str_price))
-price_without_symbol = str_price[1:] #without the pound symbol
-value = float(price_without_symbol) * float(exrate)
-# value = Decimal()
-output = round(value,2)
+
 print('\n')
-print("This music release would cost approximately -> ${} USD.".format(output))
+print('Found release price -> {} GBP.'.format(str_price))
+price_without_symbol = str_price[1:] # without the GBP symbol
+value = float(price_without_symbol) * float(exrate)
+output = round(value,2) # rounds the decimal to place 2
+print('\n')
+print("The 320 KB/S release would cost approximately -> ${} USD.".format(output))
